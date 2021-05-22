514d334b22de9efa39788331165fd27d01a0ef99
Cian Dunne <20091471@mail.wit.ie>
2021-05-22 17:26:05 +0100

    Initial Commit of Svelte Version of POI Assignment2 - currently not functioning - did function until Navigator aspects added - unsure what change caused problems

---
+++ src/App.svelte
@@ -0,0 +1,33 @@
+<script>
+  import { setContext } from "svelte";
+  import Pois from "./pages/Pois.svelte"
+  import {PoiService} from "./services/poi-service";
+  import Users from "./pages/Users.svelte";
+  import Navigator from "./components/Navigator.svelte";
+  import Router from "svelte-spa-router";
+
+  setContext("PoiService", new PoiService("http://localhost:4000"));
+
+  let routes = {
+    "/candidates": Candidates,
+    "/donations": Donations,
+  }
+
+  const navBar = [{
+    title: "Donations",
+    icon: "fas fa-hand-holding-usd fa-3x",
+    colour: "color:rgb(153, 196, 74)",
+    link: "/#/donations",
+  }, {
+    title: "Candidates",
+    icon: "fas fa-clipboard-list fa-3x",
+    colour: "color:rgb(63, 122, 139)",
+    link: "/#/candidates",
+  }];
+
+</script>
+
+<div class="uk-container">
+  <Navigator {navBar}/>
+  <Router {routes} />
+</div>
---
+++ src/components/PoiList.svelte
@@ -0,0 +1,36 @@
+<script>
+    import {onMount, getContext} from 'svelte'
+    const poiService = getContext("PoiService");
+
+
+    let poiList;
+    onMount(async () => {
+        poiList = await poiService.getPois()
+    })
+</script>
+
+<h3 class="uk-heading-divider">
+    List of POIs
+</h3>
+<div class="uk-margin uk-width-2xlarge uk-margin-auto uk-card uk-card-default uk-card-body uk-box-shadow-large">
+    <table class="uk-table">
+        <thead>
+        <th>Name</th>
+        <th>Latitude</th>
+        <th>Longitude</th>
+        <th>Description</th>
+        </thead>
+        <tbody class="uk-text-left">
+          {#if poiList}
+              {#each poiList as poi}
+                  <tr>
+                      <td>{poi.name}</td>
+                      <td>{poi.latitude}</td>
+                      <td>{poi.longitude}</td>
+                      <td>{poi.description}</td>
+                  </tr>
+              {/each}
+          {/if}
+        </tbody>
+    </table>
+</div>
